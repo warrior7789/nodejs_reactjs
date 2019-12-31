8ce53e3e37d5b070e2cb01c8dca0d02cd6ac767d
mahesh chomal <mdchomal@gmail.com>
2019-12-31 16:40:18 +0530

    latest

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParse = require("body-parser");
 const passport = require("passport");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
@@ -29,29 +29,25 @@ mongoose
 // passport middleware
 app.use(passport.initialize());
 // passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-
-
-app.get("/", (req, res) => res.send("hello i am ok"));
+//app.get("/", (req, res) => res.send("hello i am ok"));
 
 // user Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+console.log(process.env.NODE_ENV);
+//if (process.env.NODE_ENV === "production") {
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+//}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
